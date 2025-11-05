4b039628b67c974a69b501af77919ad16968b312
tjdux <tjduxxx@gmail.com>
2025-11-05 19:42:47 +0900

    파이썬을 이용한 HTML 표시, HTTPS 기초 이해

--- browser.py
+++ browser.py
@@ -1,13 +1,22 @@
 import socket
+import ssl
 
 class URL:
   # URL 파싱
   def __init__(self, url):
     self.scheme, url = url.split("://", 1)
-    assert self.scheme == "http" # http만 지원한다고 가정 
+    assert self.scheme in ["http", "https"] 
+    if self.scheme == "http":
+      self.port = 80; # http의 port: 80
+    elif self.scheme == "https":
+      self.port = 443 # https는 일반적으로 443번 포트 사용 
     if "/" not in url:
       url += "/"
     self.host, url = url.split('/', 1)
+    # 사용자 지정 포트 (url에 포트가 있다면)
+    if ":" in self.host:
+      self.host, port = self.host.split(":", 1)
+      self.port = int(port)
     self.path = "/" + url
 
   def request(self):
@@ -17,10 +26,14 @@ class URL:
       type=socket.SOCK_STREAM,
       proto=socket.IPPROTO_TCP
     )
-    s.connect((self.host, 80)) # 80번 포트 
+    # https일 시 ssl 라이브러리 사용
+    if self.scheme == "https":
+      ctx = ssl.create_default_context()
+      s = ctx.wrap_socket(s, server_hostname=self.host)
+    s.connect((self.host, self.port))
 
     # 요청 메시지 전송
-    request = f"GET {self.path} HTTP/1.0\r\n"
+    request = f"GET {self.path} HTTP/1.1\r\n"
     request += f"Host: {self.host}\r\n"
     request += "\r\n"
     s.send(request.encode("utf8"))
@@ -41,3 +54,24 @@ class URL:
     s.close()
 
     return body;
+
+# 태그를 제외한 페이지의 모든 텍스트 출력
+def show(body):
+  in_tag = False
+  for c in body:
+    if c == "<":
+      in_tag = True
+    elif c == ">":
+      in_tag = False
+    elif not in_tag:
+      print(c, end="")
+  
+# 웹페이지 로드
+def load(url):
+  body = url.request()
+  show(body)
+
+# load 함수 실행
+if __name__ == "__main__":
+  import sys
+  load(URL(sys.argv[1])) # sys.argv: 파이썬 스크립트 실행 시 전달되는 인자들의 리스트
