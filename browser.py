d3d9eab201e2dcc0fa838d70922849d112e603fe
tjdux <tjduxxx@gmail.com>
2025-11-06 15:56:29 +0900

    GUI 환경 구축 및 창 생성, 텍스트 배치 및 스크롤링 구현

--- browser.py
+++ browser.py
@@ -1,23 +1,29 @@
 import socket
 import ssl
+import tkinter
 
 class URL:
   # URL 파싱
   def __init__(self, url):
     self.scheme, url = url.split("://", 1)
+
     assert self.scheme in ["http", "https"] 
-    if self.scheme == "http":
-      self.port = 80; # http의 port: 80
-    elif self.scheme == "https":
-      self.port = 443 # https는 일반적으로 443번 포트 사용 
+    
     if "/" not in url:
       url += "/"
     self.host, url = url.split('/', 1)
+    self.path = "/" + url
+
+    if self.scheme == "http":
+      self.port = 80; # http의 port: 80
+    elif self.scheme == "https":
+      self.port = 443 # https는 일반적으로 443번 포트 사용
+
     # 사용자 지정 포트 (url에 포트가 있다면)
     if ":" in self.host:
       self.host, port = self.host.split(":", 1)
       self.port = int(port)
-    self.path = "/" + url
+    
 
   def request(self):
     # 서버에 연결
@@ -55,8 +61,9 @@ class URL:
 
     return body;
 
-# 태그를 제외한 페이지의 모든 텍스트 출력
-def show(body):
+# 태그를 제외한 페이지의 모든 텍스트 반환
+def lex(body):
+  text = ""
   in_tag = False
   for c in body:
     if c == "<":
@@ -64,14 +71,59 @@ def show(body):
     elif c == ">":
       in_tag = False
     elif not in_tag:
-      print(c, end="")
-  
-# 웹페이지 로드
-def load(url):
-  body = url.request()
-  show(body)
+      text += c
+  return text
+
+WIDTH, HEIGHT = 800, 600
+HSTEP, VSTEP = 13, 18
+
+# 각 문자의 위치를 계산하고 저장 - 페이지 좌표로 작동
+def layout(text):
+  display_list = [] # 디스플레이 리스트: 화면에 그려야 할 요소의 집합
+  cursor_x, cursor_y = HSTEP, VSTEP
+  for c in text:
+    display_list.append((cursor_x, cursor_y, c))
+    cursor_x += HSTEP
+    if (cursor_x >= WIDTH-HSTEP):
+      cursor_y += VSTEP
+      cursor_x = HSTEP
+  return display_list
+
+class Browser:
+  def __init__(self):
+    self.window = tkinter.Tk()
+    self.canvas = tkinter.Canvas(
+      self.window,
+      width=WIDTH,
+      height=HEIGHT
+    )
+    self.scroll = 0 # 스크롤한 거리
+    self.window.bind("<Down>", self.scrolldown) # bind
+    self.canvas.pack()
+
+  # 저장된 위치를 기반으로 각 문자를 그림 - 화면 좌표만 고려
+  def draw(self):
+    self.canvas.delete("all")
+    for x, y, c in self.display_list:
+      if y > self.scroll + HEIGHT: continue # 창의 아래문자 건너뛰기
+      if y + VSTEP < self.scroll: continue # 창의 위의 문자 건너뛰기
+      self.canvas.create_text(x, y-self.scroll, text=c)
+
+  # 웹페이지 로드
+  def load(self, url):
+    body = url.request()
+    text = lex(body)
+    self.display_list = layout(text)
+    self.draw()
+
+  # 스크롤 
+  def scrolldown(self, e):
+    SCROLL_STEP = 100
+    self.scroll += SCROLL_STEP
+    self.draw()
 
 # load 함수 실행
 if __name__ == "__main__":
   import sys
-  load(URL(sys.argv[1])) # sys.argv: 파이썬 스크립트 실행 시 전달되는 인자들의 리스트
+  Browser().load(URL(sys.argv[1])) # sys.argv: 파이썬 스크립트 실행 시 전달되는 인자들의 리스트
+  tkinter.mainloop()
